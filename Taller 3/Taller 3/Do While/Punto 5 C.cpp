87a5d95fe268216705347bcdce0083632296647d
felipe241920 <42384872+felipe241920@users.noreply.github.com>
2018-08-22 20:51:04 -0500

    Add files via upload

--- Taller 3/Taller 3/Do While/Punto 5 C.cpp
+++ Taller 3/Taller 3/Do While/Punto 5 C.cpp
@@ -8,17 +8,22 @@ using namespace std;
 //Funcion principal
 int main() {
 	char c;  
-	int num;
+	int num,con=0,suma=0;
 	do
 	{ 
 		printf ("introduzca un numero entero: ");
 		scanf ("%d", &num);
-		printf("\n%d * %d - %d = %d \n\n", num,num,num,num*num-num);
+		int resultado=(num*num-num);
+		con=con+1;
+		printf("\n%d * %d - %d = %d \n\n", num,num,num,resultado);
 		printf("\n deaseas ingresar otro numero (s\n)?: ");
+		suma=suma+resultado;
 		fflush (stdin);
 		scanf("%c", &c);
 		printf ("\n");
 	}while(c != 'n');
+	printf("El numero de datos ingresados es: %d\n",con);
+	printf("La suma de los ressultados es: %d",suma);
 	
 	return 0;
 }
