07f671829097cac0f61c43a3fc4bfa40c40f0448
felipe241920 <42384872+felipe241920@users.noreply.github.com>
2018-08-22 20:08:48 -0500

    Add files via upload

---
+++ Taller 3/Taller 3/Do While/Punto 5 C.cpp
@@ -0,0 +1,24 @@
+/* 
+* Programa: Operacion matematica
+* Fecha: 21-08-2018
+* Elaborado por: Felipe Henao
+*/
+#include <stdio.h>
+using namespace std;
+//Funcion principal
+int main() {
+	char c;  
+	int num;
+	do
+	{ 
+		printf ("introduzca un numero entero: ");
+		scanf ("%d", &num);
+		printf("\n%d * %d - %d = %d \n\n", num,num,num,num*num-num);
+		printf("\n deaseas ingresar otro numero (s\n)?: ");
+		fflush (stdin);
+		scanf("%c", &c);
+		printf ("\n");
+	}while(c != 'n');
+	
+	return 0;
+}
